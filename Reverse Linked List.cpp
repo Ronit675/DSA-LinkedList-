a5fffa91dd649491145db91c861d46172cc3bb4e
Ronit Jain <124519781+Ronit675@users.noreply.github.com>
2025-11-12 19:18:59 +0530

    Create Reverse Linked List.cpp

---
+++ Reverse Linked List.cpp
@@ -0,0 +1,44 @@
+// # Intuition
+// Use recursion for reversing the list
+
+// # Approach
+// -> Using recursion iterate to the last node
+// -> When pointing to last node or at null then return head
+// -> And mark it as newhead.
+// -> Now newhead is the last node and second last node is the head.
+// -> Assign head's next as front node.
+// -> And point front's next to head.
+// -> After that remove the link between front and head.
+
+// # Complexity
+// - Time complexity:
+// <!-- Add your time complexity here, e.g. $$O(n)$$ -->
+
+// - Space complexity:
+// <!-- Add your space complexity here, e.g. $$O(n)$$ -->
+
+// # Code
+// cpp []
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        ListNode* newHead = reverseList(head->next);
+        ListNode* front = head->next;
+        front->next = head;
+        head->next = NULL;
+        return newHead;
+    }
+};
