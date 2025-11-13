1206ad715daca1a6e027f09dddf3628d0a051656
Ronit Jain <124519781+Ronit675@users.noreply.github.com>
2025-11-13 12:56:48 +0530

    Refactor comments in Merge2SortedLL.cpp
    Updated comments to use C++ style and removed unnecessary HTML tags.

---
+++ Merge2SortedLL.cpp
@@ -0,0 +1,41 @@
+// # Intuition
+// <!-- Describe your first thoughts on how to solve this problem. -->
+
+// # Approach
+// <!-- Describe your approach to solving the problem. -->
+
+// # Complexity
+// - Time complexity:
+// <!-- Add your time complexity here, e.g. $$O(n)$$ -->
+
+// - Space complexity:
+// <!-- Add your space complexity here, e.g. $$O(n)$$ -->
+
+// # Code
+// ```cpp []
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == NULL || list2 == NULL){
+            return list1 == NULL ? list2:list1;
+        }
+        if(list1->val <= list2->val){
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        }
+        else{
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
+    }
+};
