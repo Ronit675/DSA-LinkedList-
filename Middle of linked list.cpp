9db7e479430eea718ac273f5711384117038e603
Ronit Jain <124519781+Ronit675@users.noreply.github.com>
2025-11-13 12:31:08 +0530

    Add middleNode function for linked list
    Implement function to find the middle node of a linked list.

---
+++ Middle of linked list.cpp
@@ -0,0 +1,37 @@
+// # Intuition
+// <!-- Describe your first thoughts on how to solve this problem. -->
+
+// # Approach
+// <!-- Describe your approach to solving the problem. -->
+
+// # Complexity
+// - Time complexity:
+// <!-- Add your time complexity here, e.g. $$O(n)$$ -->
+
+// - Space complexity:
+// <!-- Add your space complexity here, e.g. $$O(n)$$ -->
+
+// # Code
+// cpp 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+    }
+    return slow;
+    }
+};
