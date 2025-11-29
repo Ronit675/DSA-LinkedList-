53c58fbdfa99caa7a111f226b2c560db12a31b2d
Ronit Jain <124519781+Ronit675@users.noreply.github.com>
2025-11-29 21:47:16 +0530

    Add palindrome check for singly-linked list
    Implement palindrome check for a linked list using fast and slow pointers, and reverse the second half for comparison.

---
+++ Check if LL Is Palindrome or Not.cpp
@@ -0,0 +1,63 @@
+// # Intuition
+// -> First Thought is to reverse the half list and check if the it matches with the other half or not.
+
+// # Approach
+// -> Use fast and slow pointer approach to find the middle element of the list.
+// -> Reverse the other half of the list.
+// -> 
+
+// # Complexity
+// - Time complexity:
+// 
+
+// - Space complexity:
+// 
+
+// # Code
+// cpp
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+
+    ListNode* reverse(ListNode* head){
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        ListNode* newhead = reverse(head->next);
+        ListNode* front = head->next;
+        front->next = head;
+        head->next = NULL;
+        return newhead;
+    }
+    bool isPalindrome(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast->next != NULL && fast->next->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* newhead = reverse(slow->next);
+        fast = head;
+        slow = newhead;
+        while(slow != NULL){
+            if(fast->val != slow->val){
+                reverse(newhead);
+                return false;
+            }
+            fast = fast->next;
+            slow = slow->next;
+        }
+        return true;
+    }
+};
